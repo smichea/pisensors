50ad1d266aa2d9d4a604bcb20207fda48293bf42
msd-h <hhsnmsdd@gmail.com>
2020-08-28 14:47:49 +0300

    Updated bias calculator

--- src/main/c/data_fusion/BiasCalculator.cpp
+++ src/main/c/data_fusion/BiasCalculator.cpp
@@ -1,10 +1,10 @@
 #include "MPU6050_library/MPU6050.h"
 #include <time.h>
 
-MPU6050 device(0x68);
+MPU6050 mySensor(0x68);
 
 
-int totalMeasurement = 5000 ; 
+int totalMeasurement = 10000 ; 
 
 int main() {
 
@@ -13,8 +13,7 @@ int main() {
 	sleep(1); 
 
 	//Calculate the biases
-	std::cout << "Calculating the offsets...\n    Please keep the accelerometer level and still\n    This could take a couple of minutes...";
-	device.getBias(totalMeasurement,&accBias_X, &accBias_Y, &accBias_Z, &gyroBias_X, &gyroBias_Y, &gyroBias_Z);
+	mySensor.getBias(totalMeasurement,&accBias_X, &accBias_Y, &accBias_Z, &gyroBias_X, &gyroBias_Y, &gyroBias_Z);
 	std::cout << "Gyroscope X,Y,Z: " << gyroBias_X << "," << gyroBias_Y << "," << gyroBias_Z << "\n";
     std::cout << "Accelerometer X,Y,Z: " << accBias_X << "," << accBias_Y << "," << accBias_Z << "\n";
 
