adfeb68276b6f62009121684cfa3decb2714248f
msd-h <hhsnmsdd@gmail.com>
2020-08-28 14:41:40 +0300

    Removed Acc Angles from ReadRawData

--- src/main/c/data_fusion/ReadRawData.cpp
+++ src/main/c/data_fusion/ReadRawData.cpp
@@ -9,59 +9,50 @@
 MPU6050 mySensor(0x68) ;
 
 
-
-MPU6050 sensorMPU6050(0x68);
-
-
-
 int main() {
 
-	float rateX, rateY, rateZ, accX, accY, accZ; //Variables to store the accel, gyro and angle values
+	float rateX, rateY, rateZ, accX, accY, accZ; 
 		
-	float phiAcc, thetaAcc, psiAcc; 
 	
 	float dt ;
 
 	int dtMilli ;
 
-	struct timespec tS,tF ;
+	struct timespec tStart,tEnd ;
 
-	sleep(1); //Wait for the MPU6050 to stabilize
+	sleep(1); 
 
-	clock_gettime(CLOCK_REALTIME, &tS); 
+	clock_gettime(CLOCK_REALTIME, &tStart); 
 
 	while (true){
 		
 		printf("\033[H\033[J");
 		
-		sensorMPU6050.getGyro(&rateX, &rateY, &rateZ) ;
-		sensorMPU6050.getAccel(&accX, &accY, &accZ) ;
-		sensorMPU6050.getAnglesFromAcc(&accX, &accY, &accZ, &phiAcc, &thetaAcc, &psiAcc) ;
+		mySensor.getGyro(&rateX, &rateY, &rateZ) ;
+		mySensor.getAcc(&accX, &accY, &accZ) ;
 	
 		std::cout << "-------------------------------------\n" ;
 		std::cout << "-------------------------------------\n" ;
+		std::cout<<"\n" ;
 		std::cout << "Rate X: " << rateX << " deg/sec\n" ;
 		std::cout << "Rate Y: " << rateY << " deg/sec\n" ; 
 		std::cout << "Rate Z: " << rateZ << " deg/sec\n" ;
-	
-		std::cout << "Acc X: " << accX << " g\n" ;
-		std::cout << "Acc Y: " << accY << " g\n" ;
-		std::cout << "Acc Z: " << accZ << " g\n" ;
-	
-		std::cout << "\n" ;
-		std::cout << "Phi Acc: " << phiAcc << " deg\n" ;
-		std::cout << "Theta Acc: " << thetaAcc << " deg\n" ;
-		std::cout << "Psi Acc: " << psiAcc << " deg\n" ;
+		std::cout<<"\n" ;
+		std::cout << " Acc X: " << accX << " g\n" ;
+		std::cout << " Acc Y: " << accY << " g\n" ;
+		std::cout << " Acc Z: " << accZ << " g\n" ;
+		std::cout<<"\n" ;
 		
-		clock_gettime(CLOCK_REALTIME, &tF);
-		dt = (tF.tv_sec - tS.tv_sec) + (tF.tv_nsec - tS.tv_nsec) / 1e9; //Calculate new dt
+		clock_gettime(CLOCK_REALTIME, &tEnd);
+		dt = (tEnd.tv_sec - tStart.tv_sec) + (tEnd.tv_nsec - tStart.tv_nsec) / 1e9; 
 		dtMilli = round(dt * 1000) ;
 
-		std::cout << "Loop Time: " << dtMilli << " milli-second\n" ;
+		std::cout << "    dt: " << dtMilli << " milliseconds\n" ;
+		std::cout<<"\n" ;
 		std::cout << "-------------------------------------\n" ;
 		std::cout << "-------------------------------------\n" ;
 
-		clock_gettime(CLOCK_REALTIME, &tS); 
+		clock_gettime(CLOCK_REALTIME, &tStart); 
 
 	}
 
